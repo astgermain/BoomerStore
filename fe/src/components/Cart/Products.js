3ba595cd3cc4fd08ef943c625515d45952564685
Andrew St Germain <roarkyo@gmail.com>
2021-02-25 10:58:43 -0800

    Fixed deleted/deactivated product shopping cart error

--- fe/src/components/Cart/Products.js
+++ fe/src/components/Cart/Products.js
@@ -1,12 +1,18 @@
 import React, { useState, useContext } from "react";
 import Product from "./Product.js";
 import StoreContext from "../../context/store";
+import sold from "../../images/sold-out-image.webp";
 import "./cart.sass";
 
 const Products = ({ checkout }) => {
   const context = useContext(StoreContext);
   const [discountCode, setDiscountCode] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const checkoutID = context.store.checkout.id;
+  let priceCheck = false
+  const removeItem = (value) => {
+    context.removeLineItem(checkoutID, value);
+  };
   const addDiscount = () => {
     // Add a discount code to the checkout
     let message = context.addDiscount(discountCode);
@@ -18,9 +24,69 @@ const Products = ({ checkout }) => {
       <h1 className="title has-text-centered">Your Shopping Cart</h1>
       <div className="cart-products-wrapper">
         {checkout.lineItems.map((line_item) => {
-          return (
-            <Product key={line_item.id.toString()} line_item={line_item} />
-          );
+          if (line_item.variant == null) {
+            priceCheck = true
+            return (
+              <div className="cart-product-wrapper">
+                <div className="cart-product-half-1">
+                  <div className="cart-product-image">
+                    <figure
+                      className="image is-96x96"
+                      style={{ margin: "auto" }}
+                    >
+                      <img src={sold} alt="sold-out" />
+                    </figure>
+                  </div>
+                  <div className="cart-product-title-section">
+                    <p className="cart-page-product-title">{line_item.title}</p>
+                    <button
+                      className="has-text-weight-normal has-text-danger link-button reg-remove"
+                      type="button"
+                      onClick={() => removeItem(line_item.id)}
+                    >
+                      Remove Item
+                    </button>
+                  </div>
+                </div>
+                <div className="cart-product-half-1">
+                  <div
+                    style={{ verticalAlign: "inherit" }}
+                    className="unit-price reg-remove"
+                  >
+                    <span className="cart-product-column-value">This item has sold out or is unavailable</span>
+                    <span className="cart-product-column-title">
+                      You can proceed to checkout still
+                    </span>
+                  </div>
+                  <div className="unit-price reg-remove"></div>
+                  <div
+                    style={{ verticalAlign: "inherit" }}
+                    className="unit-price reg-remove"
+                  ></div>
+                  <div className="mobile-remove mobile-cart-info">
+                    <div
+                      style={{ verticalAlign: "inherit" }}
+                      className="unit-price"
+                    >This Product Has Sold Out Or Is Unavailable</div>
+                  </div>
+
+                  <div className="mobile-cart-buttons mobile-cart-info">
+                    <button
+                      className="mobile-remove button account-button4 mobile-cart-info"
+                      type="button"
+                      onClick={() => removeItem(line_item.id)}
+                    >
+                      Remove
+                    </button>
+                  </div>
+                </div>
+              </div>
+            );
+          } else {
+            return (
+              <Product key={line_item.id.toString()} line_item={line_item} />
+            );
+          }
         })}
       </div>
       {errorMessage != "" && (
@@ -50,6 +116,37 @@ const Products = ({ checkout }) => {
         </div>
         <div className="order-summary-section">
           <h1 className="order-spacing-title">Order Section</h1>
+          {priceCheck ? 
+          <>
+            <div className="order-spacing">
+            <span>Subtotal</span>
+            <span>
+            Calculated at Checkout
+            </span>
+          </div>
+          <div className="order-spacing">
+            <span>Shipping</span>
+            <span>
+            Calculated at Checkout
+            </span>
+          </div>
+          <div className="order-spacing">
+            <span>Sales Tax</span>
+            <span>Calculated at Checkout</span>
+          </div>
+          <div className="order-spacing">
+            <span>Discount</span>
+            <span>
+              Calculated at Checkout
+            </span>
+          </div>
+          <div className="order-spacing estimated-title">
+            <span>Estimated Total</span>
+            <span>Calculated at Checkout</span>
+          </div>
+          </>
+          :
+          <>
           <div className="order-spacing">
             <span>Subtotal</span>
             <span>
@@ -58,7 +155,13 @@ const Products = ({ checkout }) => {
           </div>
           <div className="order-spacing">
             <span>Shipping</span>
-            <span>{checkout?.shippingLine != null ? <> ${priceFormat(checkout?.shippingLine?.price)} </> : <>Free</>}</span>
+            <span>
+              {checkout?.shippingLine != null ? (
+                <> ${priceFormat(checkout?.shippingLine?.price)} </>
+              ) : (
+                <>Free</>
+              )}
+            </span>
           </div>
           <div className="order-spacing">
             <span>Sales Tax</span>
@@ -106,9 +209,12 @@ const Products = ({ checkout }) => {
             <span>Estimated Total</span>
             <span>${priceFormat(checkout.totalPrice)}</span>
           </div>
-          {console.log({ checkout })}
+          </>
+    }
         </div>
+                  
       </div>
+
       <div className="has-text-right checkout-button-check">
         <br />
         <a className="button checkout-button" href={checkout.webUrl}>
--- fe/src/components/HomeItems/cartSection.js
+++ fe/src/components/HomeItems/cartSection.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Link } from "gatsby"; /* eslint-disable */
 import StoreContext from "../../context/store";
+import sold from "../../images/sold-out-image.webp";
 import "./cartSection.sass";
 
 const CartSection = ({ data, quantity, cartState }) => {
@@ -8,91 +9,131 @@ const CartSection = ({ data, quantity, cartState }) => {
   //Data is allShopifyProduct
   let line = context.store.checkout.lineItems;
   let check = context.store.checkout;
-  let priceFormat = price => parseFloat(price).toFixed(2)
-
+  let priceFormat = (price) => parseFloat(price).toFixed(2);
+  let priceCheck = false;
+  line.map((l) => {
+    if (l.variant == null) {
+      priceCheck = true;
+    }
+  });
+  //
+  //
+  //
+  //NEEDS IMAGE FOR SOLD OUT
+  //
+  //
+  //
+  let soldOut = (num) => {
+    return (
+      <>
+        <div className="cart-product-box">
+          <img src={sold} className="cart-img"></img>
+          <div className="inner-cart-box">
+            <div className="small-text margin-left">
+              This item is sold out/not available
+            </div>
+            <div className="cart-product-title">{line[num]?.title}</div>
+          </div>
+          <div></div>
+        </div>
+      </>
+    );
+  };
   const firstTwoCart = () => {
     if (line.length == 0) {
       return <div>No Products In Cart</div>;
     } else if (line.length == 1) {
       return (
         <>
-          <div className="cart-product-box">
-            <img
-              src={`${line[0]?.variant.image.src}`}
-              className="cart-img"
-            ></img>
-            <div className="inner-cart-box">
-              <div className="small-text margin-left">
-                Qty: {line[0]?.quantity}
-              </div>
-              <div className="cart-product-title">
-                {line[0]?.title}
-              </div>
-            </div>
-            <div>
-              <div className="small-text float-right">
-                ${line[0]?.variant.price}
+          {line[0].variant == null ? (
+            soldOut(0)
+          ) : (
+            <div className="cart-product-box">
+              <img
+                src={`${line[0]?.variant?.image?.src}`}
+                className="cart-img"
+              ></img>
+              <div className="inner-cart-box">
+                <div className="small-text margin-left">
+                  Qty: {line[0]?.quantity}
+                </div>
+                <div className="cart-product-title">{line[0]?.title}</div>
               </div>
               <div>
-                $
-                {priceFormat(line[0]?.variant.price *
-                  line[0]?.quantity)}
+                <div className="small-text float-right">
+                  ${line[0]?.variant?.price}
+                </div>
+                <div>
+                  ${priceFormat(line[0]?.variant?.price * line[0]?.quantity)}
+                </div>
               </div>
             </div>
-          </div>
+          )}
         </>
       );
     } else {
       return (
         <>
-          <div className="cart-product-box">
-            <img
-              src={`${line[0]?.variant.image.src}`}
-              className="cart-img"
-            ></img>
-            <div className="inner-cart-box">
-              <div className="small-text margin-left">
-                Qty: {line[0]?.quantity}
-              </div>
-              <div className="cart-product-title">
-                {line[0]?.title}
-              </div>
-            </div>
-            <div>
-              <div className="small-text float-right">
-                ${line[0]?.variant.price}
-              </div>
-              <div>
-                $
-                {priceFormat(line[0]?.variant.price *
-                  line[0]?.quantity)}
-              </div>
-            </div>
-          </div>
-          <div className="cart-product-box">
-            <img
-              src={`${line[1]?.variant.image.src}`}
-              className="cart-img"
-            ></img>
-            <div className="inner-cart-box">
-              <div className="small-text margin-left">
-                Qty: {line[1]?.quantity}
-              </div>
-              <div className="cart-product-title">
-                {line[1]?.title}
-              </div>
-            </div>
-            <div>
-              <div className="small-text float-right">
-                ${line[1]?.variant.price}
+          {line[line.length - 2].variant == null ? (
+            soldOut(line.length - 2)
+          ) : (
+            <div className="cart-product-box">
+              <img
+                src={`${line[line.length - 2]?.variant?.image?.src}`}
+                className="cart-img"
+              ></img>
+              <div className="inner-cart-box">
+                <div className="small-text margin-left">
+                  Qty: {line[line.length - 2]?.quantity}
+                </div>
+                <div className="cart-product-title">
+                  {line[line.length - 2]?.title}
+                </div>
               </div>
               <div>
-                $
-                {priceFormat(line[1]?.variant.price *
-                  line[1]?.quantity)}
+                <div className="small-text float-right">
+                  ${line[line.length - 2]?.variant?.price}
+                </div>
+                <div>
+                  $
+                  {priceFormat(
+                    line[line.length - 2]?.variant?.price *
+                      line[line.length - 2]?.quantity
+                  )}
+                </div>
               </div>
             </div>
-          </div>
+          )}
+          {line[line.length - 1].variant == null ? (
+            soldOut(line.length - 1)
+          ) : (
+            <div className="cart-product-box">
+              <img
+                src={`${line[line.length - 1]?.variant?.image?.src}`}
+                className="cart-img"
+              ></img>
+              <div className="inner-cart-box">
+                <div className="small-text margin-left">
+                  Qty: {line[line.length - 1]?.quantity}
+                </div>
+                <div className="cart-product-title">
+                  {line[line.length - 1]?.title}
+                </div>
+              </div>
+              <div>
+                <div className="small-text float-right">
+                  ${line[line.length - 1]?.variant?.price}
+                </div>
+                <div>
+                  $
+                  {priceFormat(
+                    line[line.length - 1]?.variant?.price *
+                      line[line.length - 1]?.quantity
+                  )}
+                </div>
+              </div>
+            </div>
+          )}
         </>
       );
     }
@@ -103,20 +144,32 @@ const CartSection = ({ data, quantity, cartState }) => {
       <div className="small-text margin-left">Recently Added</div>
       <div>{firstTwoCart()}</div>
       <div className="cart-bottom">
-        <div>
-          <span className="small-text">Total Items </span>
-          <span>{quantity}</span>
+        <div style={{display: "flex", width: "100%", marginBottom: "5px"}}>
+          <div style={{margin: "auto"}}>
+            <span className="small-text">Total Items: </span>
+            <span>{quantity}</span>
+          </div>
+          <div style={{margin: "auto"}}>
+            <span className="small-text">Subtotal: </span>
+            {priceCheck ? (
+              <span>Recalculated At Checkout</span>
+            ) : (
+              <span>${check?.subtotalPrice}</span>
+            )}
+          </div>
         </div>
-        <div>
-          <span className="small-text">Subtotal </span>
-          <span>${check?.subtotalPrice}</span>
+        <div style={{width: "100%", display: "flex", justifyContent: "space-evenly", marginBottom: "5px"}}>
+          <Link
+            to="/cart"
+            onClick={() => cartState("")}
+            className="cart-button"
+          >
+            View Cart
+          </Link>{" "}
+          <Link to={check.webUrl} className="cart-button">
+            Checkout
+          </Link>
         </div>
-        <Link to="/cart" onClick={() => cartState("")} className="cart-button">
-          View Cart
-        </Link>{" "}
-        <Link to={check.webUrl} className="cart-button">
-          Checkout
-        </Link>
       </div>
     </div>
   );
