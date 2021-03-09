2e3f2cfbf4f3bd27c25bcb8a50019081891c512f
Andrew St Germain <roarkyo@gmail.com>
2021-03-09 14:07:42 -0800

    PLEASZE FIX

--- fe/src/components/HomeItems/AdSpace/firstAdSpace.js
+++ fe/src/components/HomeItems/AdSpace/firstAdSpace.js
@@ -1,12 +1,25 @@
 import React from "react";
-import './adSpace.sass'
+import "./adSpace.sass";
 
 const FirstAdSpace = ({ data }) => {
   //Data is allShopifyProduct
   return (
-    <div className="first-ad-space" style={{ margin: "0", display: "flex", alignItems: "flex-end", justifyContent: "center", width: "100%", height: "100%", padding: "15px" }}>
-        <button className="button account-button-ad" >Shop</button>
-    </div>
+    <a href="/" className="second-ad">
+      <div
+        className="first-ad-space"
+        style={{
+          margin: "0",
+          display: "flex",
+          alignItems: "flex-end",
+          justifyContent: "center",
+          width: "100%",
+          height: "100%",
+          padding: "15px",
+        }}
+      >
+        <button className="button account-button-ad">Shop</button>
+      </div>
+    </a>
   );
 };
 
