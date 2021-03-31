2f93cba06ae72192956b5f2abbcb596ddd047ae8
Andrew St Germain <roarkyo@gmail.com>
2021-03-31 10:37:19 -0700

    Adfasdga

---
+++ fe/src/pages/boomersupplements.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import "./boomersupplements.sass";
+
+const Supplements = () => {
+  return (
+    <div className="supplements-wrapper">
+      <div className="supplements-page">
+       </div>
+    </div>
+  );
+};
+
+export default Supplements;
