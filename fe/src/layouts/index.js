04ad717eeaf22ed2bd34ecc89c0233692d0bb45b
Andrew St Germain <roarkyo@gmail.com>
2021-04-20 10:02:14 -0700

    GTM MAIN for LEVY

--- fe/src/layouts/index.js
+++ fe/src/layouts/index.js
@@ -4,9 +4,8 @@ import Header from "../components/header";
 import Footer from "../components/footer";
 import Provider from "../context/provider";
 import Helmet from "react-helmet";
-import favicon from '../images/bsicon.png'
+import favicon from "../images/bsicon.png";
 import "./layout.sass";
-
 
 const Layout = ({ path, children, location }) => {
   const [menuMobile, setMenuMobile] = useState("");
@@ -20,6 +19,22 @@ const Layout = ({ path, children, location }) => {
   return (
     <Provider>
       <Helmet>
+        {
+          //<!-- MAIN GTM, INSTALL ON ALL PAGES -->
+          //<!-- INSTALL INSIDE <HEAD> AS HIGH AS POSSIBLE -->
+          //Google Tag Manager
+        }
+        <script>
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-MN8V8XS');`}
+        </script>
+        {/*
+      <!-- End Google Tag Manager -->
+      */}
+
         <link rel="icon" href={favicon} />
       </Helmet>
       {theme ? (
@@ -35,6 +50,16 @@ const Layout = ({ path, children, location }) => {
           }}
         />
       )}
+      {/*
+      <!-- MAIN GTM, INSTALL ON ALL PAGES -->
+      <!-- INSTALL IMMEDIATELY AFTER OPENING <BODY> TAG-->
+      <!-- Google Tag Manager (noscript) -->
+      */}
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MN8V8XS"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      {
+      //<!-- End Google Tag Manager (noscript) -->
+      }
       <div className="page-content">
         <Header
           setTheme={setTheme}
@@ -52,8 +77,12 @@ const Layout = ({ path, children, location }) => {
         >
           {" "}
         </script>
-      <script async type="text/javascript" src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=TSPD66"></script>
-    </Helmet>
+        <script
+          async
+          type="text/javascript"
+          src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=TSPD66"
+        ></script>
+      </Helmet>
     </Provider>
   );
 };
